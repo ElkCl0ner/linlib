fde50090c2dd98cadfd93966437825085278ab7c
Lin Wei Li <linwei@natosi.rogue-research.com>
2025-09-22 13:50:04 -0400

    linvector implementation

--- include/vector.hpp
+++ include/vector.hpp
@@ -7,7 +7,7 @@
 #include <unistd.h>
 #include <vector>
 
-namespace lin
+namespace ll
 {
     inline const size_t PAGE_SIZE = sysconf(_SC_PAGESIZE);
 
@@ -40,9 +40,9 @@ namespace lin
             vector()
             {
                 this->pagePtrs.push_back(this->allocPage());
-                std::cout << "PAGE SIZE: " << PAGE_SIZE << std::endl
-                          << "ELEMENT SIZE: " << ELEMENT_SIZE << std::endl
-                          << "NUM_ELEMENTS_PER_PAGE" << NUM_ELEMENTS_PER_PAGE << std::endl;
+                // std::cout << "PAGE SIZE: " << PAGE_SIZE << std::endl
+                //           << "ELEMENT SIZE: " << ELEMENT_SIZE << std::endl
+                //           << "NUM_ELEMENTS_PER_PAGE" << NUM_ELEMENTS_PER_PAGE << std::endl;
             }
 
             ~vector()
@@ -53,19 +53,55 @@ namespace lin
                 }
             }
 
-            // void append(T element)
-            // {
-            //     
-            // }
-            //
-            // T get(size_t index)
-            // {}
-            //
-            // T pop(size_t index)
-            // {}
-            //
-            // size_t find(T element)
-            // {}
+            void append(T element)
+            {
+                size_t atPage = this->numElements / NUM_ELEMENTS_PER_PAGE;
+                size_t offset = this->numElements % NUM_ELEMENTS_PER_PAGE;
+                if (offset == 0)  // new page
+                {
+                    this->pagePtrs.push_back(this->allocPage());
+                }
+                this->pagePtrs[atPage][offset] = element;
+                this->numElements++;
+            }
+
+            T* get(size_t index)
+            {
+                if (index >= this->numElements)
+                {
+                    return nullptr;
+                }
+            }
+
+            bool remove(size_t index)
+            {
+                if (index < 0 || index >= this->numElements)
+                {
+                    return false;
+                }
+                size_t atPage = index / NUM_ELEMENTS_PER_PAGE;
+                size_t offset = index % NUM_ELEMENTS_PER_PAGE;
+                size_t x_atPage = (this->numElements - 1) / NUM_ELEMENTS_PER_PAGE;
+                size_t x_offset = (this->numElements - 1) % NUM_ELEMENTS_PER_PAGE;
+                this->pagePtrs[atPage][offset] = this->pagePtrs[x_atPage][x_offset];
+                this->numElements--;
+                return true;
+            }
+
+            size_t find(T element)
+            {
+                for (int i = 0; i < this->pagePtrs.size(); i++)
+                {
+                    for (int j = 0; j < i * NUM_ELEMENTS_PER_PAGE + j; j++)
+                    {
+                        if (this->pagePtrs[i][j] == element)
+                        {
+                            return i * NUM_ELEMENTS_PER_PAGE + j;
+                        }
+                    }
+                }
+                return -1;
+            }
     };
 }
 
